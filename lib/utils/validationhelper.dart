129f13fecfe6d2f214ae29609c5895c41f896126
Lephm <quanleducpham@gmail.com>
2025-10-08 21:34:16 +1000

    add localization to validation message

--- lib/utils/validationhelper.dart
+++ lib/utils/validationhelper.dart
@@ -4,19 +4,19 @@ final RegExp emailValid = RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
 
 String? isPasswordValid(String? value, LanguageLocalizationTexts localization) {
   if (value == null || value.isEmpty) {
-    return "Please enter a password";
+    return localization.pleaseEnterAPassword;
   }
   if (value.length < 6) {
-    return "Password must be longer than 6 characters";
+    return localization.passwordMustBeLonger;
   }
   return null;
 }
 
 String? isEmailValid(String? value, LanguageLocalizationTexts localization) {
   if (value == null || value.isEmpty) {
-    return "Please enter an email";
+    return localization.pleaseEnterAnEmail;
   } else if (!emailValid.hasMatch(value)) {
-    return "Please enter a valid email";
+    return localization.pleaseEnterAValidEmail;
   }
 
   return null;
@@ -28,10 +28,10 @@ String? isTheSamePassword(
   LanguageLocalizationTexts localization,
 ) {
   if (confirmedPassword == null || confirmedPassword.isEmpty) {
-    return "Please confirm your password";
+    return localization.pleaseConfirmYourPassword;
   }
   if (confirmedPassword != password) {
-    return "Passwords do not match";
+    return localization.passwordsDoNotMatch;
   }
 
   return null;
